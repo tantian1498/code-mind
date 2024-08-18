0650b0a99a8ff1916be761f0e6824014adb3ad61
zhanfeng1 <zhanfeng1@wps.cn>
2024-08-19 00:10:19 +0800

    删除无用功能

--- src/components/mind-control.tsx
+++ src/components/mind-control.tsx
@@ -2,9 +2,6 @@ import { ReactComponent as BrushSVG } from '@/svgs/brush.svg'
 import { ReactComponent as ZoomInSVG } from '@/svgs/zoom-in.svg'
 import { ReactComponent as ZoomOutSVG } from '@/svgs/zoom-out.svg'
 import { ReactComponent as CenterSVG } from '@/svgs/center.svg'
-import { ReactComponent as BoldSVG } from '@/svgs/bold.svg'
-import { ReactComponent as ItalicSVG } from '@/svgs/italic.svg'
-import { ReactComponent as StrikeThroughSVG } from '@/svgs/strike-through.svg'
 import { containerState, controls } from '@/share'
 import { useContext, useEffect, useRef, useState } from 'react'
 import { useSelectState } from '@/hooks/useSelectState'
@@ -203,62 +200,13 @@ function Brush() {
 									</button>
 								</label>
 							</div>
-
-							<div className='border-t border-black/5' />
-
-							<div className='flex h-7 items-center justify-between gap-x-2 pl-1'>
-								<span>Fill</span>
-
-								<button className='rounded py-1 text-center hover:bg-black/5' style={{ width: 48 }}>
-									reset
-								</button>
-							</div>
-
-							<div className='border-t border-black/5' />
-
-							<div className='flex h-7 items-center justify-between gap-x-2 pl-1'>
-								<span>Text</span>
-
-								<button className='rounded py-1 text-center hover:bg-black/5' style={{ width: 48 }}>
-									reset
-								</button>
-							</div>
-							<div className='group flex h-7 items-center border border-transparent pl-1 hover:border-black/10'>
-								<div className='mr-1 h-4 w-4' style={{ backgroundColor: 'pink' }}></div>
-								<input
-									placeholder='FED2CB'
-									type='text'
-									className='h-full border-l border-r border-transparent bg-transparent px-1 py-0.5 group-hover:border-black/10'
-									style={{ width: 102 }}
-								/>
-								<input
-									type='text'
-									placeholder='100%'
-									className='h-full bg-transparent px-1 py-0.5 text-center'
-									style={{ width: 48 }}
-								/>
-							</div>
-
-							<div className='flex h-7 items-center gap-x-1'>
-								<button className='rounded p-1 text-text/60 hover:bg-black/5'>
-									<BoldSVG className='h-4 w-4' />
-								</button>
-								<button className='rounded p-1 text-text/60 hover:bg-black/5'>
-									<ItalicSVG className='h-4 w-4' />
-								</button>
-								<button className='rounded p-1 text-text/60 hover:bg-black/5'>
-									<StrikeThroughSVG className='h-4 w-4' />
-								</button>
-
-								<input className='h-full bg-transparent px-1 py-0.5' placeholder='14' style={{ width: 92 }} />
-							</div>
 						</div>
 					) : (
 						<div
 							className='absolute right-12 top-0 space-y-2 rounded-lg bg-white/80 p-3 text-xs backdrop-blur'
 							style={{ width: 200 }}>
 							<h2 className='text-center text-lg'>Null</h2>
-							<p className='text-center'>Please select the node</p>
+							<p className='text-center'>请选择一个节点</p>
 						</div>
 					))}
 			</span>
